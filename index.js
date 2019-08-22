9f05356584d1c133938378961ae23156d30e17fe
Edward Kobylyanets <kobylyanets@gmail.com>
2019-08-23 00:53:13 +0300

    fix bug heroku

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ require('dotenv').config();
 
 const Telegraf = require('telegraf');
 
+// heroku bug https://github.com/telegraf/telegraf/issues/363#issuecomment-446361074
+const express = require("express");
+const expressApp = express();
+
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
